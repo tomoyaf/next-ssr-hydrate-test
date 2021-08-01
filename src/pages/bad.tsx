bfdfbdd65764c78efbfc3bb9988d26069031b047
tomoyaf <gazimum@gmail.com>
2021-08-01 17:44:12 +0900

    fix

--- src/pages/bad.tsx
+++ src/pages/bad.tsx
@@ -1,27 +1,9 @@
-import { GetServerSideProps } from "next";
 import React from "react";
-import Simple from "components/Simple";
-import UseMemo from "components/UseMemo";
 
-export interface BadProps {
-  deadline: string;
-}
-
-export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
-  const { deadline } = process.env;
-  const defaultDeadline = "2021/01/01 00:00";
-  return {
-    props: {
-      deadline: deadline == null ? defaultDeadline : deadline,
-    },
-  };
-};
-
-const Bad: React.VFC<BadProps> = ({ deadline }) => {
+const Bad: React.VFC = () => {
   return (
     <div className="w-screen h-screen flex flex-col justify-center items-center">
-      <Simple deadline={deadline} />
-      <UseMemo deadline={deadline} />
+      <h1 className="text-9xl text-red-600">Bad</h1>
     </div>
   );
 };
--- src/pages/good.tsx
+++ src/pages/good.tsx
@@ -1,25 +1,9 @@
-import { GetServerSideProps } from "next";
 import React from "react";
-import UseEffect from "components/UseEffect";
 
-export interface BadProps {
-  deadline: string;
-}
-
-export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
-  const { deadline } = process.env;
-  const defaultDeadline = "2021/01/01 00:00";
-  return {
-    props: {
-      deadline: deadline == null ? defaultDeadline : deadline,
-    },
-  };
-};
-
-const Good: React.VFC<BadProps> = ({ deadline }) => {
+const Good: React.VFC = () => {
   return (
     <div className="w-screen h-screen flex justify-center items-center">
-      <UseEffect deadline={deadline} />
+      <h1 className="text-9xl text-blue-600">Good</h1>
     </div>
   );
 };
