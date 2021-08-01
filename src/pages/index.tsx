e03cbd3ed22dda147732c6889b84270faa5c72b7
tomoyaf <gazimum@gmail.com>
2021-08-01 15:08:04 +0900

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,29 @@
 import { GetServerSideProps } from "next";
 
 export interface HomeProps {
-  name: string;
+  deadline: string;
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
-  const { name } = process.env;
+  const { deadline } = process.env;
+  const defaultDeadline = "2021/08/01 15:10";
   return {
     props: {
-      name,
+      deadline: deadline == null ? defaultDeadline : deadline,
     },
   };
 };
 
-const Home: React.VFC<HomeProps> = ({ name }) => {
-  return <div>{name}</div>;
+const Home: React.VFC<HomeProps> = ({ deadline }) => {
+  const now = new Date();
+  const isExpired = now > new Date(deadline);
+  return (
+    <div>
+      <a href={isExpired ? "https://google.com" : "https://youtube.com"}>
+        link
+      </a>
+    </div>
+  );
 };
 
 export default Home;
